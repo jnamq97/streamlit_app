773e84dee243bc38e5032162a4accc880cbe498f
jnamq97 <namkyo0724@gmail.com>
2023-07-19 07:04:29 +0000

    warning!

--- webcam/webrtc/webcam_webrtc.py
+++ webcam/webrtc/webcam_webrtc.py
@@ -19,8 +19,8 @@ def generate_label_colors(classes=26):
 model = YOLO("/app/streamlit_app/weights/yolov8n_100epoch_.pt")
 COLORS = generate_label_colors()
 
-lock = threading.Lock()
-warning_message = {"warning": None}
+# lock = threading.Lock()
+# warning_message = {"warning": None}
 
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
@@ -51,8 +51,8 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
             warning,
         )
 
-    with lock:
-        warning_message["warning"] = warning
+    # with lock:
+    #     warning_message["warning"] = warning
 
     return av.VideoFrame.from_ndarray(image, format="bgr24")
 
@@ -74,9 +74,9 @@ def webrtc_init():
         async_processing=True,
         key="apas",
     )
-    while ctx.state.playing:
-        with lock:
-            warning = warning_message["warning"]
-        if warning != 3:
-            continue
-        st.text("warning red !!!")
+    # while ctx.state.playing:
+    #     with lock:
+    #         warning = warning_message["warning"]
+    #     if warning != 3:
+    #         continue
+    #     st.text("warning red !!!")
