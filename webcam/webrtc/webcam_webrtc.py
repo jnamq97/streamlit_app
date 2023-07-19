0da43213393f1e19b149e21a7904faa5a9dc08f6
jnamq97 <namkyo0724@gmail.com>
2023-07-19 05:59:57 +0000

    warning!

--- webcam/webrtc/webcam_webrtc.py
+++ webcam/webrtc/webcam_webrtc.py
@@ -7,6 +7,8 @@ import streamlit as st
 import av
 from ultralytics import YOLO
 import numpy as np
+import threading
+from warning_system.warning_system import warning_state_Algorithm
 
 
 # @st.cache_resource  # type: ignore
@@ -17,15 +19,19 @@ def generate_label_colors(classes=26):
 model = YOLO("/app/streamlit_app/weights/yolov8n_100epoch_.pt")
 COLORS = generate_label_colors()
 
+lock = threading.Lock()
+warning_message = {"warning": None}
+
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     image = frame.to_ndarray(format="bgr24")
-    h, w = image.shape[:2]
+    h, w = image[0].orig_shape
     preds = model(image)
 
     boxes = preds[0].boxes.boxes
     classes = preds[0].names
 
+    warning = 0
     for xmin, ymin, xmax, ymax, score, label in boxes:
         xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
         label_name = classes[int(label.item())]
@@ -40,6 +46,13 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
             color,
             2,
         )
+        warning = max(
+            warning_state_Algorithm(xmin, ymin, xmax, ymax, int(label.item()), w, h),
+            warning,
+        )
+
+    with lock:
+        warning_message["warning"] = warning
 
     return av.VideoFrame.from_ndarray(image, format="bgr24")
 
@@ -54,10 +67,16 @@ def webrtc_init():
 
     token = client.tokens.create()
 
-    webrtc_streamer(
+    ctx = webrtc_streamer(
         rtc_configuration={"iceServers": token.ice_servers},
         media_stream_constraints={"video": True, "audio": False},
         video_frame_callback=video_frame_callback,
         async_processing=True,
         key="apas",
     )
+    while ctx.state.playing:
+        with lock:
+            warning = warning_message["warning"]
+        if warning != 3:
+            continue
+        st.text("warning red !!!")
