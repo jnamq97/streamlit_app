b922bc35277d90ff14bbb26cb4b22531b1c3b3f0
jnamq97 <namkyo0724@gmail.com>
2023-07-20 10:26:52 +0000

    wf

--- webcam/webrtc/webcam_webrtc.py
+++ webcam/webrtc/webcam_webrtc.py
@@ -27,7 +27,7 @@ event_triggered = True
 box_len = 0
 lock = threading.Lock()
 # img_container = {"img": None}
-obj_contatiner = {"obj": [[] * 10000000]}
+obj_contatiner = {"obj": [[] * 1000]}
 
 
 def change_box_len():
