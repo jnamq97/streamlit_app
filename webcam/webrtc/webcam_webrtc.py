eee4b994b43c9967d2581e8e8be57af9619280e1
jnamq97 <namkyo0724@gmail.com>
2023-07-20 12:09:06 +0000

    wf

--- webcam/webrtc/webcam_webrtc.py
+++ webcam/webrtc/webcam_webrtc.py
@@ -55,7 +55,7 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
                     2,
                 )
                 danger.append(label_name)
-                obj_contatiner["obj"] = danger
+            obj_contatiner["obj"] = danger
 
     # else:
     #     obj_contatiner["obj"] = None
