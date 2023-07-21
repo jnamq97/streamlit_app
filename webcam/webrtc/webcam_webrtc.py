78b7c937b7ca9f0f10fe457ef32cbb00054c41b8
jnamq97 <namkyo0724@gmail.com>
2023-07-21 06:46:07 +0000

    ds

--- webcam/webrtc/webcam_webrtc.py
+++ webcam/webrtc/webcam_webrtc.py
@@ -39,24 +39,24 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     danger = []
 
     if len(boxes) > 0:
-        with lock:
-            for xmin, ymin, xmax, ymax, score, label in boxes:
-                xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
-                label_name = classes[int(label.item())]
-                color = COLORS[int(label.item())]
-                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-                cv2.putText(
-                    image,
-                    label_name,
-                    (xmin, ymin - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    color,
-                    2,
-                )
-                danger.append(label_name)
-            # img_container["img"] = image
-            obj_contatiner["obj"] = danger
+        for xmin, ymin, xmax, ymax, score, label in boxes:
+            xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
+            label_name = classes[int(label.item())]
+            color = COLORS[int(label.item())]
+            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+            cv2.putText(
+                image,
+                label_name,
+                (xmin, ymin - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                color,
+                2,
+            )
+            danger.append(label_name)
+    with lock:
+        # img_container["img"] = image
+        obj_contatiner["obj"] = danger
     # else:
     #     obj_contatiner["obj"] = None
     # else:
